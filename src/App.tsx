d35fba3a41ca3fd421bdbcb9d728ba0ae36b2c5d
roma1524 <kulikr.js@mail.ru>
2024-12-12 00:06:34 +0300

    fixed current project

--- src/App.tsx
+++ src/App.tsx
@@ -1,155 +1,220 @@
+import React, {useState} from 'react';
 import './App.css';
-import styled, { css } from 'styled-components';
+import {Todolist} from './Todolist';
+import {v1} from 'uuid';
 
-import mainImg from './img/mainImg.jpeg';
+
+type ObjectType = {
+    title: string
+    filter: FilterValuesType
+    tasks: Array<TasksType>
+    students: Array<string>
+}
+export type TasksType = {
+    taskId: string
+    title: string
+    isDone: boolean
+}
+
+export type FilterValuesType = "all" | "active" | "completed";
 
 
 function App() {
+
+    const [todo, setTodo] = useState<Array<ObjectType>>([
+        {
+            title: "What to learn",
+            filter: "all",
+            tasks: [
+                {taskId: v1(), title: "HTML&CSS", isDone: true},
+                {taskId: v1(), title: "JS", isDone: true}
+            ],
+            students: [
+                'Rick Kane',
+                'Finnlay Bentley',
+                'Samia North',
+                'Isaac Morton',
+                'Lily-Ann Clifford',
+                'Thalia Park',
+                'Sapphire Cruz',
+                'Cieran Vazquez',
+                'Anya Estes',
+                'Dominika Field',
+                'Rosanna Chung',
+                'Safiyah Davey',
+                'Ryley Beasley',
+                'Kalvin Trejo',
+                'Evie-Mae Farrell',
+                'Juliet Valencia',
+                'Astrid Austin',
+                'Lyle Montgomery',
+                'Nisha Mora',
+                'Kylie Callaghan',
+                'Star Wilks',
+                'Marissa Colley',
+                'Asa Fuller',
+                'Leigh Kemp',
+                'Avleen Dawson',
+                'Sammy Bonilla',
+                'Acacia Becker',
+                'Coral Shepherd',
+                'Melina Molina',
+                'Kiran Bailey',
+                'Clara Escobar',
+                'Alexandru Horn',
+                'Brandon-Lee Mercado',
+                'Elouise Weston',
+                'King Long',
+                'Kerri Searle',
+                'Kanye Hamer',
+                'Elwood Benitez',
+                'Mikail Whitaker',
+                'Bobby Hardy',
+                'Talha Ferry',
+                'Priscilla Landry',
+                'Olivia-Grace Cain',
+                'Kiaan Wallace',
+                'Wesley Padilla90',
+                'Ella-Grace Wooten91',
+                'Kaif Molloy92',
+                'Kamal Broadhurst93',
+                'Bianca Ferrell94',
+                'Micheal Talbot95',
+            ]
+        },
+        {
+            title: "What to do",
+            filter: "all",
+            tasks: [
+                {taskId: v1(), title: "HTML&CSS2", isDone: true},
+                {taskId: v1(), title: "JS2", isDone: true}
+            ],
+            students: [
+                'Jago Wormald1',
+                'Saul Milne2',
+                'Aariz Hester3',
+                'Dion Reeve4',
+                'Anisa Ortega5',
+                'Blade Cisneros6',
+                'Malaikah Phelps7',
+                'Zeeshan Gallagher8',
+                'Isobella Vo9',
+                'Rizwan Mathis10',
+                'Menaal Leach11',
+                'Kian Walton12',
+                'Orion Lamb13',
+                'Faizah Huynh14',
+                'Crystal Vaughan15',
+                'Vivien Hickman16',
+                'Stuart Lu17',
+                'Karol Davison18',
+                'Dario Burns19',
+                'Chloe Rich20',
+                'Martyna Felix',
+                'Nida Glass',
+                'Maeve Miles',
+                'Hasnain Puckett',
+                'Ayman Cano',
+                'Safwan Perry',
+                'Fox Kelly',
+                'Louise Barlow',
+                'Malaki Mcgill',
+                'Leanna Cline',
+                'Willard Hodge',
+                'Amelia Dorsey',
+                'Kiah Porter',
+                'Jeanne Daly',
+                'Mohsin Armstrong',
+                'Laurie Rangel',
+                'Princess Tierney',
+                'Kasim Kendall',
+                'Darryl Cope',
+                'Elysha Ray',
+                'Liyana Harris',
+                'Kashif Blackburn',
+                'Atif Zimmerman',
+                'Sila Hartley',
+                'Ralphie Hebert',
+            ]
+        }
+    ])
+
+    function removeTask(taskId: string, todolistId: number) {
+        setTodo(todo.map((el, index) => index === todolistId ? {
+            ...el,
+            tasks: [...el.tasks.filter(fl => fl.taskId !== taskId)]
+        } : el))
+    }
+
+    function addTask(title: string, todolistId: number) {
+
+        let newTask: TasksType = {taskId: v1(), title: title, isDone: false};
+        setTodo(todo.map((el, index) => index === todolistId ? {...el, tasks: [newTask, ...el.tasks]} : el))
+    }
+
+    function changeStatus(taskId: string, isDone: boolean, todolistId: number) {
+        setTodo(todo.map((el, index) => index === todolistId ? {
+            ...el,
+            tasks: el.tasks.map(m => m.taskId === taskId ? {...m, isDone: isDone} : m)
+        } : el))
+    }
+
+    function changeFilter(value: FilterValuesType, todolistId: number) {
+        setTodo(todo.map((el, index) => index === todolistId ? {...el, filter: value} : el))
+    }
+
+    function removeTodolist(todolistId: number) {
+        setTodo(todo.filter((el, index) => index !== todolistId))
+    }
+
+    const removeAllTodolists = () => {
+        setTodo([]);
+    }
+
+    const removeAllTasksInOneTodo = (todolistId: number) => {
+        setTodo(prevState => prevState.map((t, index) => index === todolistId
+            ? {...t, tasks: []}
+            : t
+        ))
+    }
+
     return (
-        <StyledWrapper>
-            <StyledCard>
-                <StyledImage/>
-                    <StyledDiv>
-                        <StyledTitlle>Headline</StyledTitlle>
-                        <StyledText>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</StyledText>
-                    </StyledDiv>
-                    <StyledDivForBtn>
-                        <StyledBtn bgcolor={'rgb(78, 113, 254)'} main>See more</StyledBtn>
-                        <StyledBtn textColor={'rgb(78, 113, 254)'} plain>Save</StyledBtn>
-                    </StyledDivForBtn>
-            </StyledCard>
+        <div className="App">
+            <div>
+                <button onClick={removeAllTodolists}>CLEAR ALL TODOLISTS</button>
+            </div>
 
-            <StyledCard>
-                <StyledImage/>
-                    <StyledDiv>
-                        <StyledTitlle>Headline</StyledTitlle>
-                        <StyledText>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</StyledText>
-                    </StyledDiv>
-                    <StyledDivForBtn>
-                        <StyledBtn bgcolor={'rgb(78, 113, 254)'} main>See more</StyledBtn>
-                        <StyledBtn textColor={'rgb(78, 113, 254)'} plain>Save</StyledBtn>
-                    </StyledDivForBtn>
-            </StyledCard>
+            {
+                todo.map((tl, index) => {
+                    let allTodolistTasks = tl.tasks;
+                    let tasksForTodolist = allTodolistTasks;
 
-            <StyledCard>
-                <StyledImage/>
-                    <StyledDiv>
-                        <StyledTitlle>Headline</StyledTitlle>
-                        <StyledText>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</StyledText>
-                    </StyledDiv>
-                    <StyledDivForBtn>
-                        <StyledBtn bgcolor={'rgb(78, 113, 254)'} main>See more</StyledBtn>
-                        <StyledBtn textColor={'rgb(78, 113, 254)'} plain>Save</StyledBtn>
-                    </StyledDivForBtn>
-            </StyledCard>
+                    if (tl.filter === "active") {
+                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
+                    }
+                    if (tl.filter === "completed") {
+                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
+                    }
 
-            <StyledCard>
-                <StyledImage/>
-                    <StyledDiv>
-                        <StyledTitlle>Headline</StyledTitlle>
-                        <StyledText>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</StyledText>
-                    </StyledDiv>
-                    <StyledDivForBtn>
-                        <StyledBtn bgcolor={'rgb(78, 113, 254)'} main>See more</StyledBtn>
-                        <StyledBtn textColor={'rgb(78, 113, 254)'} plain>Save</StyledBtn>
-                    </StyledDivForBtn>
-            </StyledCard>
-        </StyledWrapper>
+                    return <Todolist
+                        key={index}
+                        id={index}
+                        title={tl.title}
+                        tasks={tasksForTodolist}
+                        students={tl.students}
+                        removeTask={removeTask}
+                        changeFilter={changeFilter}
+                        addTask={addTask}
+                        changeTaskStatus={changeStatus}
+                        filter={tl.filter}
+                        removeTodolist={removeTodolist}
+                        removeAllTasksInOneTodo={removeAllTasksInOneTodo}
+                    />
+                })
+            }
+
+        </div>
     );
 }
 
 export default App;
-
-const StyledWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 20px;
-`
-
-const StyledCard = styled.div`
-    width: 300px;
-    min-height: 350px;
-    padding: 10px;
-    border-radius: 15px;
-    box-shadow: 0px 4px 20px 5px rgba(0, 0, 0, 0.1);
-    background: rgb(255, 255, 255);
-`
-
-const StyledDiv = styled.div`
-    padding: 0 10px;
-    margin-bottom: 20px;
-`
-
-const StyledImage = styled.img`
-    display: block;
-    width: 100%;
-    height: 170px;
-    background-image: url("${mainImg}");
-    margin-bottom: 20px;
-    border-radius: 10px;
-`
-
-const StyledTitlle = styled.h2`
-    font-size: 19px;
-    font-weight: 700;
-    line-height: 19px;
-    color: rgb(0, 0, 0);
-    text-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.25);
-    margin-bottom: 20px;
-    
-`
-
-const StyledText = styled.p`
-    color: rgb(171, 179, 186);    
-    font-size: 12px;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0%;
-`
-
-const StyledDivForBtn = styled.div`
-    display: flex;
-    padding: 0 10px;
-    gap: 12px;
-`
-
-type StyledBtnPropsType = {
-    textColor?: string
-    bgcolor?: string
-    main?: boolean
-    plain?: boolean
-}
-
-const StyledBtn = styled.button<StyledBtnPropsType>`
-    color:  ${props => props.textColor || 'rgb(255, 255, 255)'} ;
-    font-family: Inter;
-    font-size: 12px;
-    font-weight: 700;
-    line-height: 20px;
-    letter-spacing: 0%;
-    height: 30px;
-    min-width: 86px;
-    background-color: ${props => props.bgcolor || 'rgb(255, 255, 255)'};
-    border-radius: 5px;
-    border: 2px solid rgb(78, 113, 254);
-    cursor: pointer;
-
-
-${props => props.bgcolor && css<StyledBtnPropsType>`
-    &:hover{    
-        background-color: rgb(255, 255, 255);
-        color: rgb(78, 113, 254);
-        transition: all 0.2s;
-    }
-`}
-
-${props => props.textColor && css<StyledBtnPropsType>`
-    &:hover{    
-        background-color: rgb(78, 113, 254);
-        color: rgb(255, 255, 255);
-        transition: all 0.2s;
-    }
-`}
-`
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-// import { GlobalStyles } from './styledComponent/GlobalStyles';
-import { MicroTasks } from './components/MicroTasks';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <MicroTasks />
-    {/* <GlobalStyles/> */}
-  </React.StrictMode>
-);
+const container  = document.getElementById('root') as HTMLElement
+const root = createRoot(container);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
